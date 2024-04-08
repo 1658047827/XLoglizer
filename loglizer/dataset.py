2079f851a68dee7538d68ecce3c006c62ab5b531
Senyu Xie <1658047827@qq.com>
2024-04-08 18:45:50 +0800

    Update dataset

--- loglizer/dataset.py
+++ loglizer/dataset.py
@@ -1,46 +1,38 @@
 import torch
-import pandas as pd
+import numpy as np
 from collections import defaultdict
 from torch.utils.data import Dataset
 
 
 class LogDataset(Dataset):
     def __init__(self, session_dict):
-        # data = defaultdict(list)
-        data = []
+        data = defaultdict(list)
         for session_id, data_dict in session_dict.items():
+            window_num = len(data_dict["labels"])
             anomaly = data_dict["anomaly"]
-            for window, label in zip(data_dict["windows"], data_dict["labels"]):
-                # data["session_id"].append(session_id)
-                # data["feature"].append(window)
-                # data["label"].append(label)
-                # data["anomaly"].append(anomaly)
-                data.append({
-                    "session_id": session_id,
-                    "feature": window,
-                    "label": label,
-                    "anomaly": anomaly,
-                })
+            data["session_id"].extend([session_id] * window_num)
+            data["feature"].extend(data_dict["windows"])
+            data["label"].extend(data_dict["labels"])
+            data["anomaly"].extend([anomaly] * window_num)
+
         self.data = data
 
     def __len__(self):
-        # return len(self.data["session_id"])
-        return len(self.data)
+        return len(self.data["session_id"])
 
     def __getitem__(self, index):
-        # return {
-        #     "session_id": self.data["session_id"][index],
-        #     "feature": self.data["feature"][index],
-        #     "label": self.data["label"][index],
-        #     "anomaly": self.data["anomaly"][index],
-        # }
-        return self.data[index]
+        return (
+            self.data["session_id"][index],
+            self.data["feature"][index],
+            self.data["label"][index],
+            self.data["anomaly"][index],
+        )
 
 
 def log_collate(batch):
     return {
-        "session_id": [sample["session_id"] for sample in batch],
-        "feature": torch.tensor([sample["feature"] for sample in batch], dtype=torch.float),
-        "label": torch.tensor([sample["label"] for sample in batch]),
-        "anomaly": [sample["anomaly"] for sample in batch],
+        "session_id": [sample[0] for sample in batch],
+        "feature": torch.tensor([sample[1] for sample in batch], dtype=torch.float),
+        "label": torch.tensor([sample[2] for sample in batch]),
+        "anomaly": [sample[3] for sample in batch],
     }
