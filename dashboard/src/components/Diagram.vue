18a0d3159b5a8c28556537ff136ad1e92e2324de
Senyu Xie <1658047827@qq.com>
2024-04-17 19:26:18 +0800

    Better force simulation

--- dashboard/src/components/Diagram.vue
+++ dashboard/src/components/Diagram.vue
@@ -12,72 +12,81 @@ const width = 1080
 const height = 720
 
 onMounted(() => {
+    const simulation = d3.forceSimulation(data.nodes)
+        .force("link", d3.forceLink(data.links).id(d => d.id))
+        .force("charge", d3.forceManyBody().strength(-400))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("x", d3.forceX())
+        .force("y", d3.forceY())
+        .on("tick", ticked);
+
+    // Create the SVG container.
     const svg = d3.select("#diagram-container")
         .append("svg")
-        .attr('viewBox', `0 0 ${width} ${height}`)
-        .attr('width', width)
-        .attr('height', height);
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto; font: 12px sans-serif;");
 
-    const simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(d => d.id))
-        .force("charge", d3.forceManyBody().strength(-400))
-        .force("center", d3.forceCenter(width / 2, height / 2));
-
+    // Add a line for each link, and a circle for each node.
     const link = svg.append("g")
-        .attr("class", "links")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
         .selectAll("line")
         .data(data.links)
-        .enter()
-        .append("line")
-        .attr("fill", "none")
-        .attr("stroke-width", d => d.width) // edge width
-        .attr("stroke", "gray")
+        .join("line")
+        .attr("stroke-width", d => d.width * 3);
 
     const node = svg.append("g")
-        .attr("class", "nodes")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
         .selectAll("circle")
         .data(data.nodes)
-        .enter().append("circle")
+        .join("circle")
         .attr("r", d => d.size)
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+        .attr("fill", "#ff3399");
 
     node.append("title")
         .text(d => d.id);
 
-    simulation.nodes(data.nodes)
-        .on("tick", ticked);
+    // Add a drag behavior.
+    node.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
 
-    simulation.force("link")
-        .links(data.links);
-
+    // Set the position attributes of links and nodes each time the simulation ticks.
     function ticked() {
-        link.attr("x1", d => d.source.x)
+        link
+            .attr("x1", d => d.source.x)
             .attr("y1", d => d.source.y)
             .attr("x2", d => d.target.x)
             .attr("y2", d => d.target.y);
 
-        node.attr("cx", d => d.x)
+        node
+            .attr("cx", d => d.x)
             .attr("cy", d => d.y);
     }
 
-    function dragstarted(event, d) {
+    // Reheat the simulation when drag starts, and fix the subject position.
+    function dragstarted(event) {
         if (!event.active) simulation.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
     }
 
-    function dragged(event, d) {
-        d.fx = event.x;
-        d.fy = event.y;
+    // Update the subject (dragged node) position during drag.
+    function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
     }
 
-    function dragended(event, d) {
+    // Restore the target alpha so the simulation cools after dragging ends.
+    // Unfix the subject position now that it’s no longer being dragged.
+    function dragended(event) {
         if (!event.active) simulation.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
+        event.subject.fx = null;
+        event.subject.fy = null;
     }
 })
 
@@ -88,6 +97,6 @@ onMounted(() => {
     cursor: pointer;
     fill: #ff3399;
     stroke: #000;
-    stroke-width: .5px;
+    stroke-width: 1px;
 }
 </style>
