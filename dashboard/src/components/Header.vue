7ff84bcf4df0ac2c390697a4386bcbd7d636fe67
Senyu Xie <1658047827@qq.com>
2024-04-19 22:56:00 +0800

    Finish header for dashboard

---
+++ dashboard/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header-container">
+        <span class="title">XLoglizer</span>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.header-container {
+    background-color: #409EFF;
+    height: 60px;
+    display: flex;
+    align-items: center;
+}
+
+.header-container .title {
+    font-size: xx-large;
+    color: white;
+    margin-left: 20px;
+}
+</style>
