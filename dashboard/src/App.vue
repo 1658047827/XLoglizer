b83ba5e9e6f603609626c28410fa6216558968e1
Senyu Xie <1658047827@qq.com>
2024-04-20 14:34:15 +0800

    Finish basic framework of index

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
+import Header from './components/Header.vue';
+import Predict from './components/Predict.vue';
 import Diagram from './components/Diagram.vue';
 </script>
 
 <template>
-    <Diagram></Diagram>
+    <div class="container" style="background: #fafafa; height: 100%;">
+        <el-container>
+            <el-header style="padding: 0;">
+                <Header></Header>
+            </el-header>
+            <el-main style="padding: 0; display: flex;">
+                <Predict></Predict>
+                <Diagram :width="960" :height="720"></Diagram>
+            </el-main>
+        </el-container>
+    </div>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body,
+#app {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+}
+</style>
