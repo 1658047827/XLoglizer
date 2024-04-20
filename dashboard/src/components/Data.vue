d4ece55d55cc08a355264db10c404f5543830056
Senyu Xie <1658047827@qq.com>
2024-04-21 01:21:01 +0800

    Data.vue now can show trace and input

--- dashboard/src/components/Data.vue
+++ dashboard/src/components/Data.vue
@@ -1,40 +1,47 @@
 <template>
     <div>
         <el-table :data="tableData" style="width: 100%">
-            <el-table-column prop="date" label="Date" width="180" />
-            <el-table-column prop="name" label="Name" width="180" />
-            <el-table-column prop="address" label="Address" />
+            <el-table-column prop="input" label="Input" width="350">
+                <template #default="{ row }">
+                    {{ showInput(row.input) }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="trace" label="Trace" width="180">
+                <template #default="{ row }">
+                    {{ showTrace(row.trace) }}
+                </template>
+            </el-table-column>
         </el-table>
         <el-pagination layout="prev, pager, next" :total="1000" />
     </div>
 </template>
 
 <script setup>
+
+const showInput = (input) => {
+    let formattedString = "";
+    for (let i = 0; i < input.length - 1; i++) {
+        formattedString += `E${input[i]}`;
+        formattedString += ", ";
+    }
+    formattedString += `E${input[input.length - 1]}`;
+    return formattedString;
+}
+
+const showTrace = (trace) => {
+    let formattedString = "";
+    for (let i = 0; i < trace.length - 1; i++) {
+        formattedString += `S${trace[i]}`;
+        formattedString += '→';
+    }
+    formattedString += `S${trace[trace.length - 1]}`;
+    return formattedString;
+}
+
 const tableData = [
     {
-        date: '2016-05-03',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-    {
-        date: '2016-05-02',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-    {
-        date: '2016-05-04',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-    {
-        date: '2016-05-01',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-    {
-        date: '2016-05-03',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
+        input: [22, 5, 5, 5, 26, 26, 26, 11, 9, 11],
+        trace: [0, 1, 2, 3],
     },
 ]
 </script>
