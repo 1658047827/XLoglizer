ff54c2419a38a5046bc0ed69b694a77ffc657074
Senyu Xie <1658047827@qq.com>
2024-04-20 21:12:22 +0800

    Fix template.py by adding E0 to output

--- data/HDFS_v1/template.py
+++ data/HDFS_v1/template.py
@@ -4,4 +4,6 @@ import pandas as pd
 if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.abspath(__file__))
     df = pd.read_csv(f"{file_dir}/preprocessed/HDFS.log_templates.csv")
-    json_data = df.to_json(f"{file_dir}/output.json", orient="records")
+    padding = pd.DataFrame({"EventId": ["E0"], "EventTemplate": ["padding"]})
+    df = pd.concat([padding, df]).reset_index(drop=True)
+    json_data = df.to_json(f"{file_dir}/templates.json", orient="records")
