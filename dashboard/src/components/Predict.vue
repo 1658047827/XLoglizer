d0112ba6535e6e671465bb74515e72b39a87593f
Senyu Xie <1658047827@qq.com>
2024-05-03 14:51:16 +0800

    Simply view

--- dashboard/src/components/Predict.vue
+++ dashboard/src/components/Predict.vue
@@ -4,19 +4,7 @@
         <!-- <el-button type="danger" plain @click="clear">Clear</el-button> -->
         <el-card style="width: 500px; margin-top: 10px;">
             <div style="display: flex; justify-content: space-between; align-items: center;">
-                <div>
-                    <el-tag v-for="eid in eids" :key="eid" closable :disable-transitions="false"
-                        @close="handleClose(eid)" style="margin-right: 10px; margin-bottom: 10px;">
-                        E{{ eid }}
-                    </el-tag>
-                    <el-input v-if="inputVisible" ref="InputRef" v-model="inputValue" size="small"
-                        @keyup.enter="handleInputConfirm" @blur="handleInputConfirm" style="margin-top: 10px;">
-                        <template #prefix>E</template>
-                    </el-input>
-                    <el-button v-else size="small" @click="showInput" style="margin-bottom: 10px;">
-                        + New Event
-                    </el-button>
-                </div>
+                <el-input v-model="inputValue" type="textarea" style="margin-right: 20px;"></el-input>
                 <el-button :icon="Search" circle @click="predict" />
             </div>
         </el-card>
@@ -26,7 +14,7 @@
                 <el-table-column prop="EventTemplate" label="EventTemplate" width="1000" />
             </el-table>
         </div>
-        <!-- <div style="font-size: medium; margin-top: 10px;">RNN Loglizer Top K Prediction</div> -->
+        <div style="font-size: large; margin-top: 15px; font-weight: bold;">Loglizer Top K Prediction</div>
         <div id="chart-container">
             <div style="width: 100%; height: 300px" ref="chartRef"></div>
         </div>
@@ -41,44 +29,20 @@
 <script setup>
 import { Search } from '@element-plus/icons-vue'
 import axios from "axios";
-import { nextTick, onMounted, ref } from "vue";
+import { onMounted, ref } from "vue";
 import * as echarts from 'echarts';
 
 const templates = ref([])
 const trace = ref([0])
-const inputValue = ref("")
-const eids = ref([22, 5, 5, 5, 26, 26, 26, 11, 9, 11])
-const inputVisible = ref(false)
-const InputRef = ref()
-
-const handleClose = (eid) => {
-    eids.value.splice(eids.value.indexOf(eid), 1)
-}
-
-const showInput = () => {
-    inputVisible.value = true
-    nextTick(() => {
-        InputRef.value.input.focus()
-    })
-}
-
-const handleInputConfirm = () => {
-    if (inputValue.value) {
-        eids.value.push(Number(inputValue.value))
-    }
-    inputVisible.value = false
-    inputValue.value = ""
-}
-
-// const clear = () => {
-//     eids.value = []
-// }
+const inputValue = ref("E22,E5,E5,E5,E26,E26,E26,E11,E9,E11")
 
 const predict = async () => {
-    if (eids.value.length !== 10) {
+    const separated = inputValue.value.split(",");
+    const eids = separated.map((eid) => Number(eid.slice(1)))
+    if (eids.length !== 10) {
         ElMessage.error("Please enter a sequence of length 10.")
     } else {
-        const data = { data: eids.value }
+        const data = { data: eids }
         const resp = await axios.post("http://localhost:5000/predict", data)
         pieData = resp.data.topk_pred
         option.series[0].data = pieData
@@ -110,9 +74,6 @@ onMounted(async () => {
 
     chart = echarts.init(chartRef.value);
     option = {
-        title: {
-            text: "RNN Loglizer Top K Prediction"
-        },
         series: [
             {
                 type: 'pie',
@@ -132,8 +93,4 @@ onMounted(async () => {
 })
 </script>
 
-<style scoped>
-#chart-container {
-    margin-top: 20px;
-}
-</style>
+<style scoped></style>
